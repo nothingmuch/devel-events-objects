740489a32628cd351b5db28ec1f986796cb0cb6e
Yuval Kogman <nothingmuch@woobling.org>
2007-09-12 23:48:22 +0000

    Document Generator::Events

--- lib/Devel/Events/Generator/Objects.pm
+++ lib/Devel/Events/Generator/Objects.pm
@@ -175,4 +175,101 @@ __PACKAGE__;
 
 __END__
 
+=pod
 
+=head1 NAME
+
+Devel::Events::Generator::Objects - Generate events for C<bless>ing and
+destruction of objects.
+
+=head1 SYNOPSIS
+
+	use Devel::Events::Generator::Objects; # must be loaded before any code you want to instrument
+
+	my $g = Devel::Events::Generator::Objects->new(
+		handler => $h,
+	);
+
+	$g->enable(); # only one Objects generator may be enabled at a time
+
+	$code->(); # objects being created and destroyed cause events to be generated
+
+	$g->disable();
+
+=head1 DESCRIPTION
+
+This module overrides C<CORE::GLOBAL::bless> on load. The altered version will
+delegate back to the original version until an instance of a generator is enabled.
+
+When a generator is enabled (only one L<Devel::Events::Generator::Objects>
+instance may be enabled at a time. Use L<Devel::Events::Handler::Multiplex> to
+dup events to multiple listeners), the overridden version of C<bless> will
+cause an C<object_bless> event to fire, and will also attach magic to the
+object to keep track of it's destruction using L<Variable::Magic>.
+
+When the object is freed by the interpreter an C<object_destroy> event is
+fired. Unfortunately by this time C<perl> has already unblessed the object in
+question, so in order to keep track of the class you must associate it yourself
+with the reference address.
+
+L<Devel::Events::Handler::ObjectTracker> contains a detailed usage example.
+
+=head1 METHODS
+
+=over 4
+
+=item enable
+
+Make this instance the enabled one (disabling any other instance which is
+enabled).
+
+This only applies to the C<object_bless> method.
+
+=item disable
+
+Disable this instance. Will stop generating C<object_bless> events.
+
+=item bless
+
+The method called by the C<CORE::GLOBAL::bless> hook.
+
+Uses C<CORE::bless> to bless the data, and then calls C<object_bless>.
+
+=item object_bless
+
+Generates the C<object_bless> event.
+
+Calls C<rack_object>.
+
+=item object_destroy
+
+Generates the C<object_destroy> event.
+
+Calls C<untrack_object>.
+
+=item generate_event
+
+Convenience method used by the previous two methods.
+
+=item tracker_magic
+
+A class method containing the L<Variable::Magic> specification necessary for
+L<track_object> to work.
+
+=item track_object
+
+Attach magic to an object that will call C<object_destroy> when the data is
+about to be freed.
+
+=item untrack_object
+
+Currently empty. A subclass with a different implementation of C<track_object>
+might want to override this.
+
+=back
+
+=head1 SEE ALSO
+
+L<Devel::Object::Leak>, L<Variable::Magic>
+
+=cut
