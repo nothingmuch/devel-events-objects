d606b73352ccef3108f5a2f654e87d67474221bd
Yuval Kogman <nothingmuch@woobling.org>
2007-09-12 22:55:14 +0000

    more doc work

--- lib/Devel/Events/Objects.pm
+++ lib/Devel/Events/Objects.pm
@@ -61,6 +61,33 @@ Devel::Events::Objects - Object tracking support for L<Devel::Events>
 This package provides an event generator and a handler for L<Devel::Events>,
 that facilitate leak checking.
 
+There are two components of this module: L<Devel::Events::Generator::Objects>,
+and L<Devel::Events::Handler::ObjectTracker>.
+
+The first one uses some trickery to generate events for every object creation
+and destruction in code loaded after it was loaded.
+
+The second one will listen on these events, and track all currently living
+objects.
+
+See the L</SYOPSIS> for how to write your own leak tracker, and
+L<Catalyst::Plugin::LeakTracker> for a real world application of these classes.
+
+=head1 SEE ALSO
+
+L<Devel::Events>, L<Devel::Events::Filter::Size>,
+L<Catalsyt::Plugin::LeakTracker>, L<Devel::Cycle>, L<Devel::Leak::Object>
+
+=head1 AUTHOR
+
+Yuval Kogman <nothingmuch@woobling.org>
+
+=head1 COPYRIGHT & LICENSE
+
+	Copyright (c) 2007 Yuval Kogman. All rights reserved
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the MIT license or the same terms as Perl itself.
+
 =cut
 
 
